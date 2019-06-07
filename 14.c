a964937109e0bf93cd3e4cee8e35fa1af22fc597
阿部慎之介 <shin@abeshinnosukenoMacBook-Pro.local>
2019-06-07 16:17:58 +0900

    main作成

--- 14.c
+++ 14.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-//test
-//
-//test3
-//test4
-int main(){
 
+#define NUM_OF_PRINCESSES 4
+#define NUM_OF_MIDDLE_PRINCESSES 3
+#define NUM_OF_LAST_PRINCESSES 1
+
+int main(){
+  for(;;){
+    int s
+    printf("１：ゲームルール表示\n");
+    printf("２：ゲームスタート\n");
+    printf("上記の中から数字を選択してください：")
+    scanf("%d",&s)
+    if(s == 1){
+      rule();//ルールを表示する関数
+    }else if(s == 2){
+      game_start();
+    }
+  }
+  return 0;
 }
