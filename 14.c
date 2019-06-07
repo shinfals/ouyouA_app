61c4c2ab4bda4a61cfba8eb86d37905719b2dd6c
阿部慎之介 <shin@abeshinnosukenoMacBook-Pro.local>
2019-06-07 16:24:20 +0900

    main修正

--- 14.c
+++ 14.c
@@ -4,13 +4,16 @@
 #define NUM_OF_MIDDLE_PRINCESSES 3
 #define NUM_OF_LAST_PRINCESSES 1
 
+rule();
+game_start();
+
 int main(){
   for(;;){
     int s
     printf("１：ゲームルール表示\n");
     printf("２：ゲームスタート\n");
-    printf("上記の中から数字を選択してください：")
-    scanf("%d",&s)
+    printf("上記の中から数字を選択してください：");
+    scanf("%d",&s);
     if(s == 1){
       rule();//ルールを表示する関数
     }else if(s == 2){
