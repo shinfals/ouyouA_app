9f94c93e1bdbe6091bff3245d1429a5fd6f1ff1a
阿部慎之介 <shin@abeshinnosukenoMacBook-Pro.local>
2019-06-07 17:49:56 +0900

    構造体追加

--- 14.c
+++ 14.c
@@ -5,10 +5,60 @@
 #define NUM_OF_LAST_PRINCESSES 1
 
 void game_rule();
-void initialize();
+void dungeon_initialize();
+void char_initialize();
+void end_initialize();
 void game_start();
 
 int main(){
+  typedef struct Tokimeki_dungeon{
+    int PrincessEnemyNum;
+    int TresureChest;
+    int Door[4];
+  }Tokimeki_dungeon;
+
+  typedef struct Princess_enemy{
+    cahr name[256];
+    int Hp;
+    int Mp;
+    int Atk;
+    int Def;
+    int Spd;
+    int Skill;
+  }Princess_enemy;
+
+  typedef struct Prince{
+    char naem[] = "Prince";
+    int Hp;
+    int Mp;
+    int Atk;
+    int Def;
+    int Spd;
+    int Skill;
+    int flag = 0;
+    int x = 0;
+    int y = 0;
+  }Prince;
+
+  typedef struct Princess_support{
+    int Hp;
+    int Atk;
+    int Def;
+    int Spd;
+  }Princess_support;
+
+  typedef struct Weapon{
+    char name[256];
+    int Atk;
+    int Def;
+    int Skill;
+  }Weapon;
+
+  typedef struct Item{
+    int Hp;
+    int Mp
+  }Item;
+
   for(;;){
     int s;
     printf("１：ゲームルール表示\n");
@@ -18,7 +68,9 @@ int main(){
     if(s == 1){
       game_rule();//ルールを表示する関数
     }else if(s == 2){
-      initialize();//初期化関数
+      dungeon_initialize(Tokimeki_dungeon);//ダンジョン情報初期化
+      char_initialize(Princess_enemy,Princess_support);//キャラクター初期化
+      end_initialize();//終了条件初期化
       game_start();//ゲーム本体の関数
     }
   }
@@ -29,10 +81,6 @@ void game_rule(){
 
 }
 
-void initialize(){
-  
-}
-
-void game_start(){
-
+void end_initialize(){
+  end_flag = 0;
 }
