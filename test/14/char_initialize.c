0fb2eb5757378caf4a91e0dfce75174b8a5763e1
tsuyuki1223 <rufi1223@yahoo.co.jp>
2019-06-21 15:01:48 +0900

    prince randam化

--- test/14/char_initialize.c
+++ test/14/char_initialize.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include "func.h"
 void char_initialize(Prince *pPri){
-	pPri->name[256]="Prince";
-	pPri->Hp=1000;
-	pPri->Mp=1000;
-	pPri->Atk=500;
-	pPri->Def=500;
-	pPri->Spd=5;
-	pPri->Skill=1;
-	pPri->flag=0;
-	pPri->x=0;
-	pPri->y=0;
+	int hp = 0;
+	int mp = 0;
+	int at = 0;
+	int df = 0;
+	int sp = 0;
+
+	hp = rand()%701+500;
+	mp = rand()%6+5;
+	at = rand()%151+150;
+	df = rand()%151+150;
+	sp = rand()%5+1;
+
+	pPri>-name[256]="Prince";
+	pPri>-Hp=hp;
+	pPri>-Mp=mp;
+	pPri>-Atk=at;
+	pPri>-Def=df;
+	pPri>-Spd=sp;
+	pPri>-Skill=1;
+	pPri>-flag=0;
+	pPri>-x=0;
+	pPri>-y=0;
 }
